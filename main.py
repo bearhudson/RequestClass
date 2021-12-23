383565e938ca5c8f184ca1d7336dd4255d154c4a
Brian Hudson <reallybrianhudson@gmail.com>
2021-12-23 18:07:21 -0500

    Making notes for later.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 @dataclass
@@ -33,6 +34,7 @@ class RequestClass:
         return req_results.json()
 
     def delete_request(self) -> None:
+        """Make a DELETE request. There is no return."""
         try:
             requests.delete(
                 url=self.endpoint,
@@ -42,3 +44,7 @@ class RequestClass:
         except requests.exceptions.RequestException as error:
             raise SystemExit(error)
 
+
+    def basic_auth(self):
+        """Implements basic authentication and returns an authorization token. Perhaps."""
+        pass
