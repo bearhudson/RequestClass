dadad275256a6cbca4c6eb7b5a461ccae0d88a9e
Brian Hudson <reallybrianhudson@gmail.com>
2021-12-23 17:56:35 -0500

    Testing

--- main.py
+++ main.py
@@ -10,12 +10,35 @@ class RequestClass:
     json: dict = None
     data: dict = None
     cookies: str = None
-
+    auth: str = None
+    timeout: int = None
+    allow_redirects: bool = None
+    proxies = str = None
+    hooks: str = None
+    stream: str = None
+    verify: str = None
+    cert: str = None
 
     def post_request(self) -> dict:
-        results = {}
+        """Make a POST request and return a dictionary."""
+        req_results = {}
         try:
-            req = requests.post(url=self.endpoint, )
+            req_results = requests.post(
+                url=self.endpoint,
+                params=self.parameters,
+                headers=self.headers,
+            )
         except requests.exceptions.RequestException as error:
             raise SystemExit(error)
-        return results
+        return req_results.json()
+
+    def delete_request(self) -> None:
+        try:
+            requests.delete(
+                url=self.endpoint,
+                params=self.parameters,
+                headers=self.headers,
+            )
+        except requests.exceptions.RequestException as error:
+            raise SystemExit(error)
+
